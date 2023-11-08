33d2bcfd9c21a60445ab6673afc22e330e83de23
Antal <43848302+alkenderesi@users.noreply.github.com>
2023-11-08 15:06:17 +0100

    Refactor scripts

--- script.js
+++ script.js
@@ -1,127 +1,160 @@
+const body = document.querySelector('body');
+const main = document.querySelector('main');
+
+
+/**
+* Save the complete HTML of the item list as a string (including child elements).
+*/
 function updateLocalStorage() {
-    const itemList = document.getElementById('item-list');
-    const items = itemList.getElementsByTagName('li');
-    const htmlString = itemList.outerHTML;
-    localStorage.setItem('itemList', htmlString);
-    let categories = []
-    for (let i = 0; i < items.length; i++) {
-        const input = items[i].querySelector('input[type="text"]');
-        categories.push(input.value)
-    }
-    localStorage.setItem('categories', categories);
+
+    localStorage.setItem('items', main.innerHTML);
+
+    // For some reason, the raw HTML does not contain
+    // the value attribute of the input elements.
+    // We must save those separately.
+
+    const values = Array.from(main.querySelectorAll('input[type="text"]')).map(input => input.value);
+    localStorage.setItem('values', values);
 }
 
+/**
+* Create a new list item HTML element.
+*/
 function createItem() {
-    const item = document.createElement('li')
+    const item = document.createElement('li');
 
-    const itemInfo = document.createElement('div')
-    itemInfo.setAttribute('class', 'item-info')
-    item.appendChild(itemInfo)
+    const itemInfo = document.createElement('div');
+    itemInfo.setAttribute('class', 'item-info');
+    item.appendChild(itemInfo);
 
-    const itemText = document.createElement('input')
-    itemText.setAttribute('type', 'text')
-    itemInfo.appendChild(itemText)
+    const itemText = document.createElement('input');
+    itemText.setAttribute('type', 'text');
+    itemInfo.appendChild(itemText);
 
-    const removeButton = document.createElement('button')
-    removeButton.setAttribute('title', 'remove item')
-    removeButton.innerText = 'X'
-    itemInfo.appendChild(removeButton)
+    const removeButton = document.createElement('button');
+    removeButton.setAttribute('title', 'remove item');
+    removeButton.innerText = 'X';
+    itemInfo.appendChild(removeButton);
 
-    const itemWeight = document.createElement('div')
-    itemWeight.setAttribute('class', 'item-weight')
-    item.appendChild(itemWeight)
+    const itemWeight = document.createElement('div');
+    itemWeight.setAttribute('class', 'item-weight');
+    item.appendChild(itemWeight);
 
-    const marble = document.createElement('div')
-    itemWeight.appendChild(marble)
+    const marble = document.createElement('div');
+    itemWeight.appendChild(marble);
 
-    return item
+    return item;
 }
 
+/**
+* Add a new item to the list.
+*/
 function addItem() {
-    document.getElementById('item-list').appendChild(createItem())
-    updateLocalStorage()
+    main.querySelector('ol').appendChild(createItem());
 }
 
+/**
+* Remove an item from the list based on which button was clicked.
+* 
+* Attempting to remove the last item triggers a warning alert.
+*/
 function removeItem(event) {
-    if (event.target.tagName === 'BUTTON' && event.target.title === 'remove item') {
-        const itemList = document.getElementById('item-list');
-        const itemCount = itemList.getElementsByTagName('li').length;
+    if (event.type === 'click' && event.target.title === 'remove item') {
+
+        const itemCount = main.querySelectorAll('li').length;
 
         if (itemCount > 1) {
             event.target.closest('li').remove();
-            updateLocalStorage()
         } else {
             alert('Cannot remove the last item!');
         }
     }
 }
 
+/**
+* Select a random item from the list.
+*
+* Items with more weight have a higher probability of being chosen.
+*/
 function marbleRaffle() {
-    const itemList = document.getElementById('item-list');
-    const items = itemList.getElementsByTagName('li');
 
-    // Build weighted list
+    // Build weighted list.
+    const items = main.querySelectorAll('li');
     let weightedList = [];
-    for (let i = 0; i < items.length; i++) {
-        const marbles = items[i].querySelectorAll('.item-weight div');
-        for (let j = 0; j < marbles.length; j++) {
-            weightedList.push(i)
-        }
-    }
 
-    // Pick a random item
-    const randomIndex = weightedList[Math.floor(Math.random() * weightedList.length)]
+    items.forEach((item, i) => {
+        const marbles = item.querySelectorAll('.item-weight div');
+        marbles.forEach(() => {
+            weightedList.push(i);
+        });
+    });
+
+    // Pick a random item.
+    const randomIndex = weightedList[Math.floor(Math.random() * weightedList.length)];
     const chosenItem = items[randomIndex];
 
-    // Remove all but one marble from the chosen item
-    const chosenItemMarbles = chosenItem.querySelectorAll('.item-weight div');
-    for (let i = chosenItemMarbles.length - 1; i > 0; i--) {
-        chosenItemMarbles[i].remove();
+    // Remove all marbles from the chosen item.
+    const marbles = chosenItem.querySelectorAll('.item-weight div');
+    for (let i = marbles.length - 1; i >= 0; i--) {
+        marbles[i].remove();
     }
 
-    // Add one marble to all other items
-    for (let i = 0; i < items.length; i++) {
-        if (i !== randomIndex) {
-            const weight = items[i].querySelector('.item-weight');
-            const marble = document.createElement('div');
-            weight.appendChild(marble);
-        }
-    }
+    // Add one marble to all items.
+    items.forEach((item, i) => {
+        const weight = item.querySelector('.item-weight');
+        const marble = document.createElement('div');
+        weight.appendChild(marble);
+    });
 
-    // Show an alert with the value of the chosen item
+    // Show an alert with the value of the chosen item.
     const value = chosenItem.querySelector('input[type="text"]').value;
     alert(value);
-    updateLocalStorage()
 }
 
+/**
+* Create a new list with one item.
+*/
 function createDefaultItemList() {
-    const itemList = document.createElement('ol')
-    itemList.setAttribute('id', 'item-list')
-    itemList.appendChild(createItem())
+    const items = document.createElement('ol');
+    items.appendChild(createItem());
 
-    return itemList
+    return items;
 }
 
+/**
+* Reset the item list to the default state.
+*/
 function resetList() {
-    const itemList = document.getElementById('item-list')
-    const newItemList = createDefaultItemList()
-    itemList.parentNode.replaceChild(newItemList, itemList)
-    updateLocalStorage()
+    const items = main.querySelecto('ol');
+    const newItems = createDefaultItemList();
+    items.parentNode.replaceChild(newItems, items);
 }
 
-const itemListString = localStorage.getItem('itemList');
-const categoryList = localStorage.getItem('categories').split(',');
+/**
+* Load saved item list (and event listeners) on page reload.
+*/
+function loadList() {
+    const items = localStorage.getItem('items');
 
-if (itemListString !== null) {
-    document.getElementById('main').innerHTML = itemListString
-    const inputFields = document.querySelectorAll('input[type="text"]')
-    for (let i = 0; i < inputFields.length; i++) {
-        inputFields[i].setAttribute('value', categoryList[i])
+    if (items !== null) {
+    
+        main.innerHTML = items;
+    
+        const inputs = main.querySelectorAll('input[type="text"]');
+        const values = localStorage.getItem('values').split(',');
+    
+        inputs.forEach((input, i) => {
+            input.setAttribute('value', values[i]);
+        });
+    
+    } else {
+        main.appendChild(createDefaultItemList());
     }
-} else {
-    document.getElementById('main').appendChild(createDefaultItemList())
+    
+    body.addEventListener('click', removeItem);
+    body.addEventListener('click', updateLocalStorage);
+    body.addEventListener('input', updateLocalStorage);
 }
 
-document.getElementById('main').addEventListener('click', removeItem);
-document.getElementById('main').addEventListener('click', updateLocalStorage);
-document.getElementById('main').addEventListener('input', updateLocalStorage);
+
+loadList()
